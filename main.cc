37939920572abe6656329b448ce8cbcc44935c56
Jayden <jayden.hong@goat.com>
2023-01-04 13:20:59 +0800

    init

---
+++ main.cc
@@ -0,0 +1,19 @@
+//
+// Created by Jayden on 2023/1/4.
+//
+
+#include "rocksdb/db.h"
+
+
+int main() {
+    rocksdb::DB* db;
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    rocksdb::Status status = rocksdb::DB::Open(options, "/Users/jayden/github/forgoes/research-rocksdb/db/test", &db);
+
+    if (!status.ok()) {
+        printf("open db failed: %s", status.ToString().c_str());
+        return 1;
+    }
+    return 0;
+}
