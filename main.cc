eae18de17ef5bc4e93c6bbafaa51bf49c487852b
Jayden <hongkang@hongkang.name>
2023-04-27 15:42:11 +0800

    chore

--- main.cc
+++ main.cc
@@ -16,6 +16,14 @@ int main() {
         return 1;
     }
 
+    std::string value;
+    rocksdb::Status s = db->Put(rocksdb::WriteOptions(), "key1", "222222");
+    if (s.ok()) s = db->Get(rocksdb::ReadOptions(), "key1", &value);
+    if (s.ok()) s = db->Put(rocksdb::WriteOptions(), "key2", value);
+    std::string got;
+    if (s.ok()) s = db->Get(rocksdb::ReadOptions(), "key2", &got);
+    if (s.ok()) s = db->Delete(rocksdb::WriteOptions(), "key1");
+
     printf("open db successfully");
 
     return 0;
